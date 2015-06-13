f7686c55060136869ac5cb5fb74d71aaefe0822d
Kminkjan <kminkjan@gmail.com>
2015-06-13 14:10:23 +0200

    Clarifying

--- agario-client/RoboBot.js
+++ agario-client/RoboBot.js
@@ -19,54 +19,8 @@ client.connect("ws://127.0.0.1:9158/");
 client.on('connected', function () { //when we connected to server
     client.log('spawning');
     client.spawn('[xFake]'); //spawning new ball
-    //interval_id = setInterval(recalculateTarget, 100); //we will search for target to eat every 100ms
     interval_id = setInterval(update, 100);
 });
-
-function recalculateTarget() { //this is all our example logic
-    var running = false;
-    var run_x = [];
-    var run_y = [];
-    var bad_boys = [];
-    // TODO add with orientation
-
-    var candidate_ball = null; //first we don't have candidate to eat
-    var candidate_distance = 0;
-    var my_ball = client.balls[client.my_balls[0]]; //we get our first ball. We don't care if there more then one, its just example.
-    if (!my_ball) return; //if our ball not spawned yet then we abort. We will come back here in 100ms later
-
-    for (var ball_id in client.balls) { //we go true all balls we know about
-        var ball = client.balls[ball_id];
-        if (ball.virus) continue; //if ball is a virus (green non edible thing) then we skip it
-        if (!ball.visible) continue; //if ball is not on our screen (field of view) then we skip it
-        if (ball.mine) continue; //if ball is our ball - then we skip it
-
-        /* Calculate evasiveness */
-        var distance = getDistanceBetweenBalls(ball, my_ball); //we calculate distances between our ball and candidate
-        if (ball.size / my_ball.size > 1.1) {
-            if (distance < ball.size * 3) {
-                run_x.push(my_ball.x - (ball.x - my_ball.x));
-                run_y.push(my_ball.y - (ball.y - my_ball.y));
-                running = true;
-                console.log("running from: " + ball);
-            }
-            continue;
-        } else if (ball.size / my_ball.size > 0.7) continue; // No threat
-
-        if (candidate_ball && distance > candidate_distance) continue; //if we do have some candidate and distance to it smaller, than distance to this ball, we skip it
-        candidate_ball = ball; //we found new candidate and we record him
-        candidate_distance = getDistanceBetweenBalls(ball, my_ball); //we record distance to him to compare it with other balls
-    }
-    if (!candidate_ball) return; //if we didn't find any candidate, we abort. We will come back here in 100ms later
-
-    if (running) {
-        var x = average(run_x);
-        var y = average(run_y);
-        client.moveTo(x, y);
-    } else {
-        client.moveTo(candidate_ball.x, candidate_ball.y); //we send move command to move to food's coordinates
-    }
-}
 
 function update() {
     var my_ball = client.balls[client.my_balls[0]];
@@ -95,8 +49,11 @@ function update() {
         }
     }
 
+    /* Save all coordinates to run to */
     var run_x = [];
     var run_y = [];
+
+    /* Save the unsafe directions */
     var bad_directions = [];
 
     /* Process threats */
